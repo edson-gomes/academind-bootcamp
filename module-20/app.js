5d7f19167b63fc2f76fbe2466ff180a0dd7631f5
Edson Gomes <73556396+edson-gomes@users.noreply.github.com>
2022-01-17 10:56:32 -0300

    Finished lectures from Day 53 and Module 20
    Explored more features of the 'Express Router' and finished the basic back-end concepts.

--- module-20/app.js
+++ module-20/app.js
@@ -1,8 +1,9 @@
-const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
-const uuid = require("uuid");
+
+const defaultRoutes = require("./routes/default");
+const restaurantRoutes = require("./routes/restaurants");
 
 const app = express();
 
@@ -12,56 +13,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-  res.render("index");
-});
-
-app.get("/restaurants", function (req, res) {
-  const restaurants = getStoredRestaurants();
-
-  res.render("restaurants", {
-    numberOfRestaurants: restaurants.length,
-    restaurants: restaurants,
-  });
-});
-
-app.get("/restaurants/:id", function (req, res) {
-  const restaurantId = req.params.id;
-
-  const restaurants = getStoredRestaurants();
-
-  for (const restaurant of restaurants) {
-    if (restaurant.id === restaurantId) {
-      return res.render("restaurant-detail", { restaurant: restaurant });
-    }
-  }
-
-  res.status(404).render("404");
-});
-
-app.get("/recommend", function (req, res) {
-  res.render("recommend");
-});
-
-app.post("/recommend", function (req, res) {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-
-  const restaurants = getStoredRestaurants();
-
-  restaurants.push(restaurant);
-
-storeRestaurants(restaurants)
-  res.redirect("/confirm");
-});
-
-app.get("/confirm", function (req, res) {
-  res.render("confirm");
-});
-
-app.get("/about", function (req, res) {
-  res.render("about");
-});
+app.use("/", defaultRoutes);
+app.use("/", restaurantRoutes);
 
 app.use(function (req, res) {
   res.status(404).render("404");
